2d7731b6e1957fbb2fab77aa99e64c3afd424afb
Brian Birir <brianbirir@gmail.com>
2019-01-10 16:32:00 +0300

    Add description of problem statement for reverse words

--- reverse_word.py
+++ reverse_word.py
@@ -1,3 +1,8 @@
+'''
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+'''
+
+
 def reverse_words(text):
     
     words = text.split(' ')
