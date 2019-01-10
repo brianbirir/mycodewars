5e38dc7afb0465d9a1beb3b31c90d9ee612af469
Brian Birir <brianbirir@gmail.com>
2019-01-10 16:30:15 +0300

    Add code for solving reverse words

---
+++ reverse_word.py
@@ -0,0 +1,28 @@
+def reverse_words(text):
+    
+    words = text.split(' ')
+    
+    new_sentence = []
+    
+    for w in words:
+  
+        # string to list i.e. array
+        new_word = list(w)
+
+        # get number of letters in word
+        word_length = len(w)
+
+        # initial positions
+        start = 0
+        end = word_length - 1
+
+        while start < end:
+
+            new_word[start], new_word[end] = new_word[end], new_word[start]
+            start += 1
+            end -= 1
+            
+        # return "".join(new_word)
+        new_sentence.append("".join(new_word))
+    
+    return " ".join(new_sentence)
